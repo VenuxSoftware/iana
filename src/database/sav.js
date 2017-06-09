c9cb30d6a4acb29171f288c48c78cf424376a789
Eugene Nosko <coder@venux.com>
2017-06-08 20:18:05 -0400

    Database - SAV

--- src/database/sav.js
+++ src/database/sav.js
@@ -1,15 +1,18 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+'use strict'
+var npa = require('npm-package-arg')
 
-/*---
-description: Async test
-negative: RangeError
-expected:
-  pass: true
----*/
-
-setTimeout(function() {
-    $DONE(new RangeError());
-}, 1000);
+module.exports = function (name, sw, where) {
+  try {
+    if (sw.version && sw.integrity) {
+      return npa.resolve(name, sw.version, where)
+    }
+    if (sw.resolved) {
+      return npa.resolve(name, sw.resolved, where)
+    }
+    if (sw.from) {
+      var spec = npa(sw.from, where)
+      if (!spec.registry) return spec
+    }
+  } catch (_) { }
+  return npa.resolve(name, sw.version, where)
+}
