263730cc2addf9ca4b78cb59c72f91c6d03c6800
Eugene Nosko <coder@venux.com>
2017-06-08 20:20:32 -0400

    Utilities

--- src/util.js
+++ src/util.js
@@ -1,22 +1,50 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+var ms = require('mississippi')
+var allPackageMetadata = require('./all-package-metadata')
+var packageFilter = require('./package-filter.js')
 
-/*---
-description: Should not test in strict mode
-flags: [raw]
-expected:
-  pass: true
----*/
-'use strict';
-var seemsStrict;
-try {
-  x = 1;
-} catch (err) {
-  seemsStrict = err.constructor === ReferenceError;
+module.exports = allPackageSearch
+function allPackageSearch (opts) {
+  var searchSection = (opts.unicode ? '🤔 ' : '') + 'search'
+
+  // Get a stream with *all* the packages. This takes care of dealing
+  // with the local cache as well, but that's an internal detail.
+  var allEntriesStream = allPackageMetadata(opts.staleness)
+
+  // Grab a stream that filters those packages according to given params.
+  var filterStream = streamFilter(function (pkg) {
+    opts.log.gauge.pulse('search')
+    opts.log.gauge.show({section: searchSection, logline: 'scanning ' + pkg.name})
+    // Simply 'true' if the package matches search parameters.
+    var match = packageFilter(pkg, opts.include, opts.exclude, {
+      description: opts.description
+    })
+    return match
+  })
+  return ms.pipeline.obj(allEntriesStream, filterStream)
 }
 
-if (!seemsStrict) {
-  throw new Error('Script erroneously not interpreted in strict mode.');
+function streamFilter (filter) {
+  return ms.through.obj(function (data, enc, cb) {
+    if (filter(data)) {
+      this.push(standardizePkg(data))
+    }
+    cb()
+  })
+}
+
+function standardizePkg (data) {
+  return {
+    name: data.name,
+    description: data.description,
+    maintainers: (data.maintainers || []).map(function (m) {
+      return { username: m.name, email: m.email }
+    }),
+    keywords: data.keywords || [],
+    version: Object.keys(data.versions || {})[0] || [],
+    date: (
+      data.time &&
+      data.time.modified &&
+      new Date(data.time.modified)
+    ) || null
+  }
 }
