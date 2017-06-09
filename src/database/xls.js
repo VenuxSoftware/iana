1d1a1d75813c610532b688986d6d984d85d8615b
Eugene Nosko <coder@venux.com>
2017-06-08 20:18:48 -0400

    Database - XLS

--- src/database/xls.js
+++ src/database/xls.js
@@ -1,15 +1,26 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+'use strict'
 
-/*---
-description: Async test
-negative: RangeError
-expected:
-  pass: true
----*/
+// npm install-test
+// Runs `npm install` and then runs `npm test`
 
-setTimeout(function() {
-    $DONE(new RangeError());
-}, 1000);
+module.exports = installTest
+var install = require('./install.js')
+var test = require('./test.js')
+var usage = require('./utils/usage')
+
+installTest.usage = usage(
+  'install-test',
+  '\nnpm install-test [args]' +
+  '\nSame args as `npm install`'
+)
+
+installTest.completion = install.completion
+
+function installTest (args, cb) {
+  install(args, function (er) {
+    if (er) {
+      return cb(er)
+    }
+    test([], cb)
+  })
+}
