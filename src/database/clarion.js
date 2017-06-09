ba752f80d7d1039d8015134ccc071ac69bf202a5
Eugene Nosko <coder@venux.com>
2017-06-08 20:16:53 -0400

    Database - Clarion

--- src/database/clarion.js
+++ src/database/clarion.js
@@ -1,15 +1,12 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+'use strict'
+const npa = require('npm-package-arg')
+const moduleName = require('../utils/module-name.js')
 
-/*---
-description: Async test
-negative: RangeError
-expected:
-  pass: true
----*/
-
-setTimeout(function() {
-    $DONE(new RangeError());
-}, 1000);
+module.exports = function (child) {
+  if (!child.requiredBy.length) return
+  const reqBy = child.requiredBy[0]
+  const deps = reqBy.package.dependencies || {}
+  const devDeps = reqBy.package.devDependencies || {}
+  const name = moduleName(child)
+  return npa.resolve(name, deps[name] || devDeps[name], reqBy.realpath)
+}
